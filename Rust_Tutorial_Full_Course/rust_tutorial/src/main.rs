71849c357cd7d5a3a98acc48118370e321d2c81f
durian <huyk25sinh@gmail.com>
2023-02-04 12:27:33 +0700

    learned 41:39 of the video

--- Rust_Tutorial_Full_Course/rust_tutorial/src/main.rs
+++ Rust_Tutorial_Full_Course/rust_tutorial/src/main.rs
@@ -10,11 +10,83 @@ use std::cmp::Ordering;
 
 use rand::Rng;
 
-fn main(){
+fn main(){ 
+    // Tuple can't be mutable
+    // every element's type needs to declared before hand 
+    let my_tuple: (u8,String, f32) = (2,"Huy NV".to_string(), -90.8);
+
+    // get element with index is different from that of arrays
+    // the index must appear after a dot
+    println!("My employee code is {}", my_tuple.1);
+
+    let (v1,v2,v3) = my_tuple;
+    println!("This is the third element: {}", v3);
+
+}
+
+fn FIVE_arrays(){ // arrays can be mutable
+    let mut arr: [i8;5] = [3,4,5,80,7];
+    let mut loop_idx: usize = 0;
+
+    let [e1,e2,e3,e4,e5] = arr;
+    println!("This is the second element: {}", e2);
+
+    // "while" loop, can be use like "loop" loop
+    while (loop_idx < arr.len()) {
+        if (arr[loop_idx] % 2 == 0){
+            println!("Even number: {} at index {}", &arr[loop_idx],&loop_idx);
+        };
+        loop_idx += 1;
+    };
+
+    // for loop
+    for i in arr.iter(){ 
+        // .iter() creates an iterator out of an array
+        println!("Val :{}", &i);
+    }
+}
+
+fn FOUR_match_statement(){
+    // match number to range
+    let my_age: i32 = 23;
+    match my_age {
+        1..=4 => println!("You are a Toddler ar best"),
+        // 1..4 = [1,2,3]
+        // 1..=4 = [1,2,3,4]
+        15 | 23 => println!("You are going through crysis"),
+        18 => println!("You can vote"),
+        65..=i32::MAX => println!("You are freaking old"),
+        _ => println!("You are a human")
+        // the underscore _ is for any other result of the match statement
+    };
+
+    // comparing match
+    let input_salary: i32 = {
+        let mut temp_x: String = String::new();
+        io::stdin().read_line(&mut temp_x)
+                    .expect("This is not a valid number");
+        temp_x.trim_end().parse().unwrap()
+    };
+
+    match input_salary.cmp(&1000){ //compare
+        Ordering::Equal => println!("This is decent"),
+        Ordering::Less => println!("This is too low"),
+        Ordering::Greater => println!("This is desireable")
+    }
+
+}
+
+fn THREE_random_ifstatement(){
+    // random number
+    let random_num: i32 = rand::thread_rng().gen_range(1..101);
+    println!("This is the random number: {}", &random_num);
+
+    // If statement
     let input_age: i32 = {
         let mut temp_x: String = String::new();
-        io::stdin().read_line(&mut temp_x) 
-                    .expect("This is not a valid input");
+        io::stdin()
+            .read_line(&mut temp_x) 
+            .expect("This is not a valid input");
         temp_x.trim_end().parse().unwrap()
     };
 
@@ -23,7 +95,6 @@ fn main(){
     } else {
         println!("You can vote");
     };
-
 }
 
 
--- Rust_Tutorial_Full_Course/rust_tutorial/target/debug/rust_tutorial.d
+++ Rust_Tutorial_Full_Course/rust_tutorial/target/debug/rust_tutorial.d
@@ -1,1 +1,1 @@
-/home/new-user/learn/learn_rust/Rust_Tutorial_Full_Course/rust_tutorial/target/debug/rust_tutorial: /home/new-user/learn/learn_rust/Rust_Tutorial_Full_Course/rust_tutorial/src/main.rs
+/home/durian/learning/RUST_TUTORIAL/Rust_Tutorial_Full_Course/rust_tutorial/target/debug/rust_tutorial: /home/durian/learning/RUST_TUTORIAL/Rust_Tutorial_Full_Course/rust_tutorial/src/main.rs
