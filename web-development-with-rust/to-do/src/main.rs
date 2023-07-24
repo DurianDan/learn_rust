7ec15398528fee842b463c8ec6e34e00b9e28258
Huy Nguyễn Vũ <durian@mark2>
2023-07-25 00:06:03 +0700

    Resolve #1 understand move keyword and closure and borrowing

--- web-development-with-rust/to-do/src/main.rs
+++ web-development-with-rust/to-do/src/main.rs
@@ -2,10 +2,15 @@ mod modules;
 mod config;
 
 use crate::modules::Status;
-use actix_web::{HttpServer, HttpResponse, App, web, Responder};
+use actix_web::{
+    HttpServer,
+    HttpResponse,
+    App,
+    web,
+    Responder};
 use std::io;
 use dotenv::dotenv;
-
+use tokio_postgres::NoTls;
 
 async fn status() -> impl Responder{
     HttpResponse::Ok()
@@ -18,11 +23,18 @@ async fn main() -> io::Result<()>{
     dotenv().ok();
     
     let config = crate::config::Config::from_env().unwrap();
+    let pool = config.pg.create_pool(NoTls).unwrap();
+    // this is local dev, so connecting to the db with TLS isn't necessary
 
     println!("Starting the server at http://{}:{}", config.server.host, config.server.port);
     
-    HttpServer::new(|| { 
+    HttpServer::new(move || {
+        // the move keyword can only be used with closures
+        // If any var (of outside scope) appears in the closure, it's automatically borrowed.
+        // using the move keyword will ensure the closure actually owns them
+        // in this case, the `App` will be created multiple time, so the `pool` must also be duplicated, borrowing the same var might lead to unexpected behaviors.
         App::new()
+            .app_data(pool.clone()) // this var will be accessable in other actix functions
             .route("/", web::get().to(status))
     })
     .bind(format!("{}:{}", config.server.host, config.server.port))?
